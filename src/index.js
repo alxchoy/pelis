322d07403a939e690b1ee91be74910e1e47766b9
Choy <alexanderchoy@multiplica.com>
2017-10-02 15:46:49 -0500

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import AppRoutes from './router.js';
-import './index.css';
-
-ReactDOM.render(<AppRoutes />, document.getElementById('root'));
