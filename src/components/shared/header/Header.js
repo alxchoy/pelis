1eb035fead979838acbb72586cc5b9c1ac3a9f2e
Choy <alexanderchoy@multiplica.com>
2017-10-02 15:29:36 -0500

    cambios header links

--- src/components/shared/header/Header.js
+++ src/components/shared/header/Header.js
@@ -7,7 +7,7 @@ class Header extends React.Component {
 		return (
 			<header>
 				<div className="header-container">
-					<div className="logo"><Link to="/">FullPelis</Link></div>
+					<div className="logo"><Link to="/pelis/">FullPelis</Link></div>
 					<div className="filtro">
 						<ul>
 							<li><Link to="/pelis/mas-votados">Más votados</Link></li>
